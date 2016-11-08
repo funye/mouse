7d0a777100abd29eaef8177212ad7be01564948c
funye <fun913510024@163.com>
2016-11-08 16:17:47 +0800

    change the file name

--- src/main/java/com/fun/ui/MouseFrame.java
+++ src/main/java/com/fun/ui/MouseFrame.java
@@ -1,88 +1,95 @@
 package com.fun.ui;
 
-import com.fun.http.HttpServer;
+import com.fun.utils.DateUtil;
 import com.fun.utils.Language;
 
 import javax.swing.*;
-import java.awt.*;
+import javax.swing.text.BadLocationException;
+import javax.swing.text.Document;
+import javax.swing.text.SimpleAttributeSet;
+import javax.swing.text.StyleConstants;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
 /**
- * Created by yehuan on 2016/11/7.
- *
+ * Created by yehuan on 2016/11/8.
  */
-public class MouseFrame extends JFrame{
-
-    private static final int WIDTH=400;
-    private static final int HEIGHT=200;
-
-    private JButton btn;
-    private JTextArea logArea;
+public class MouseFrame {
     private JTextField hostInput;
     private JTextField portInput;
-
-    private HttpServer server;
-    private Thread serverThread;
-
-
-    public MouseFrame(String title) {
-
-        setTitle(title);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new FlowLayout());
-        setSize(WIDTH,HEIGHT);
-        setLocation(750,300);
-
-        JLabel host = new JLabel(Language.get("ui.host"));
-        host.setHorizontalAlignment(SwingConstants.RIGHT);
-        hostInput = new JTextField(15);
-
-        JLabel port = new JLabel(Language.get("ui.port"));
-        port.setHorizontalAlignment(SwingConstants.RIGHT);
-        portInput = new JTextField(15);
-
-        btn = new JButton(Language.get("ui.start"));
-        btn.addActionListener(new MyClickListener());
-
-        logArea = new JTextArea("",15,35);
-        logArea.setMargin(new Insets(0,0,15,0));
-
-        JPanel p0 = new JPanel();
-        p0.add(host);
-        p0.add(hostInput);
-
-        JPanel p1 = new JPanel();
-        p1.add(port);
-        p1.add(portInput);
-
-        JPanel p2 = new JPanel();
-        p2.add(btn);
-
-        JPanel p3 = new JPanel();
-        p3.add(logArea);
-
-        add(p0);
-        add(p1);
-        add(p2);
-        add(p3);
-
-    }
-
-
-    public void showUI(){
-        setVisible(true);
-    }
+    private JButton btn;
+    private JScrollPane logScrollPane;
+    private JPanel mainPanel;
+    private JTextPane logPane;
+    private JButton btnClear;
 
     public static void main(String[] args) {
-        new MouseFrame(Language.get("ui.title")).showUI();
+
+        MouseFrame f = new MouseFrame();
+        f.launch();
     }
 
-    class MyClickListener implements ActionListener {
+    public void launch() {
 
+        JFrame jFrame= new JFrame(Language.get("ui.title"));
+
+        jFrame.setContentPane(mainPanel);
+        jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.pack();
+        jFrame.setSize(800, 600);
+        jFrame.setLocationRelativeTo(mainPanel);//居中
+        jFrame.setVisible(true);
+
+        MyListener listener = new MyListener();
+
+        btn.addActionListener(listener);
+        btnClear.addActionListener(listener);
+    }
+
+    public class MyListener implements ActionListener {
         @Override
         public void actionPerformed(ActionEvent e) {
-            System.out.println("click...");
+
+            JButton button = (JButton)e.getSource();
+            if(button == btn){
+                String host = hostInput.getText();
+                String portStr = portInput.getText();
+
+                host = host == null ? "192.168.1.119":host;
+                int port = portStr == null ? 8080:Integer.parseInt(portStr);
+
+                log("click the btn with text [ " +button.getText()+ " ]");
+                if(btn.getText().equals(Language.get("ui.start"))){ // 启动
+                    log("it't going to start httpserver on [ "+host+" ] with port [ "+port+" ]");
+                    // to start server
+
+                    btn.setText(Language.get("ui.stop"));
+                }else{// 关闭
+                    log("it't going to stop httpserver on [ "+host+" ] with port [ "+port+" ]");
+                    // to stop server
+
+                    btn.setText(Language.get("ui.start"));
+                }
+            }else if(button == btnClear){
+                logPane.setText("");
+            }else{
+                log("这是测试消息，source="+e.getSource() + ", ======  id=" + e.getID());
+            }
         }
     }
+
+    public void log(String msg) {
+        //设置字体大小
+        SimpleAttributeSet attrset = new SimpleAttributeSet();
+        StyleConstants.setFontSize(attrset,12);
+        //插入内容
+        Document docs = logPane.getDocument();//获得文本对象
+        String insert = "[ "+ DateUtil.currentDate()+" ]\t"+msg+"\n";
+        try {
+            docs.insertString(docs.getLength(), insert,attrset);//对文本进行追加
+        } catch (BadLocationException ex) {
+            ex.printStackTrace();
+        }
+    }
+
 }
